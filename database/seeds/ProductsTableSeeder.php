9dc4c163cf14b5cb9d24d643bdf0aec85158813f
Unknown <karoljesionkadev@gmail.com>
2018-06-16 18:19:53 +0200

    Create Product seeder

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Product;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Product::truncate();
+
+        $products = [
+            ['Product 1', 4],
+            ['Product 2', 12],
+            ['Product 5', 0],
+            ['Product 7', 6],
+            ['Product 8', 2],
+        ];
+
+        foreach($products as $product) {
+            Product::create([
+                'name' => $product[0],
+                'amount' => $product[1]
+            ]);
+        }
+    }
+}
